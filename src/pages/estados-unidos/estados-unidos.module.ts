8fc407d34c6a90a375ea8752dc4347f961496681
carolcezareti <36717652+carolcezareti@users.noreply.github.com>
2018-05-27 18:27:03 -0300

    Initial commit

---
+++ src/pages/estados-unidos/estados-unidos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EstadosUnidosPage } from './estados-unidos';
+
+@NgModule({
+  declarations: [
+    EstadosUnidosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EstadosUnidosPage),
+  ],
+})
+export class EstadosUnidosPageModule {}
